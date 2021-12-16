b5ed215d75307a613306b6005f8e1cde211f965f
lyzdyl <yili@lyzd.com>
2021-12-16 14:51:39 +0800

    first commit

---
+++ src/main/java/com/sgg/GitTest/TestGit.java
@@ -0,0 +1,12 @@
+package com.sgg.GitTest;
+
+/**
+ * @author : yili
+ * @date : 2021/12/16
+ * @description :
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
